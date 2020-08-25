24bbfcfc6fd53f1c6b8d6472255359e9585947a7
Idris.Wonuola <Idris.Wonuola@wemabank.com>
2020-08-25 15:24:05 +0100

    Add project files.

---
+++ PersonRepository.Factory/RepositoryFactory.cs
@@ -0,0 +1,28 @@
+﻿using PersonRepository.CSV;
+using PersonRepository.Interface;
+using PersonRepository.Service;
+using PersonRepository.SQL;
+using System;
+
+namespace PersonRepository.Factory
+{
+	public class RepositoryFactory
+	{
+		public static IPersonRepository GetRepository(string repositroyType)
+		{
+			IPersonRepository repository = null;
+			switch (repositroyType)
+			{
+				case "Service": repository = new ServiceRepository();
+					break;
+				case "CSV": repository = new CSVRepository();
+					break;
+				case "SQL":repository = new SQLRepository();
+					break;
+				default:
+					throw new ArgumentException("Invalid repository type");
+			}
+			return repository;
+		} 
+	}
+}
